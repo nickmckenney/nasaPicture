58e7e13ecdd6d8f2e09c8e1ddf389696ab31ad12
NickMckenney <mckenney2001@gmail.com>
2019-12-23 15:59:55 -0500

    changed port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const parser = require("body-parser");
 app.use(parser.json());
 app.use(cors());
 app.use("/", require("./routes/pictureRoutes"));
-app.set("port", process.env.PORT || 4000);
+app.set("port", process.env.PORT || 8080);
 
 app.listen(app.get("port"), () => {
   console.log(`✅ PORT: ${app.get("port")} 🌟`);
