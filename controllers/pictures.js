890c95922e82df83c2b18e3d71334b642eb620b5
NickMckenney <mckenney2001@gmail.com>
2020-01-12 15:46:35 -0500

    made changes to seed.js. deleted content

--- controllers/pictures.js
+++ controllers/pictures.js
@@ -5,13 +5,7 @@ const stateController = {
       res.json(states);
     });
   },
-  edit: (req, res) => {
-    Nasa.findOneAndUpdate({ name: req.params.name }, req.body, {
-      new: true
-    }).then(states => {
-      res.json(states);
-    });
-  },
+
   showNasaName: (req, res) => {
     Nasa.findOne({ name: req.params.name }).then(states => {
       res.json(states);
