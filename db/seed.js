aa4716744dd2f98f7f5d1f461c67aedfedb3666c
NickMckenney <mckenney2001@gmail.com>
2020-01-12 15:30:29 -0500

    deleted data file

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,5 @@
 const Picture = require("../models/nasaSchema.js");
-const data = require("./data.json");
+// const data = require("./data.json");
 Picture.deleteMany({}).then(() => {
   Picture.create(data).then(programsArray => {
     console.log(programsArray);
