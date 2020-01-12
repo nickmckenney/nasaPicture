460d91010ec343702bd2fe95d725ebbdf8554028
NickMckenney <mckenney2001@gmail.com>
2020-01-12 15:41:33 -0500

    deleted seed.js file

--- db/getPictures.js
+++ db/getPictures.js
@@ -16,11 +16,12 @@
 //     });
 //   });
 const fetch = require("node-fetch");
+
 const url =
   "https://api.nasa.gov/planetary/apod?api_key=GJ6Jvh1c8erMgH5edhmmQ538fNHcSbldUeAc68AK";
 
 fetch(url)
   .then(res => res.json())
   .then(res => {
-    console.log(res);
+    console.log(res.url);
   });
--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,7 @@
-const Picture = require("../models/nasaSchema.js");
-// const data = require("./data.json");
-Picture.deleteMany({}).then(() => {
-  Picture.create(data).then(programsArray => {
-    console.log(programsArray);
-  });
-});
+// const Picture = require("../models/nasaSchema.js");
+// // const data = require("./data.json");
+// Picture.deleteMany({}).then(() => {
+//   Picture.create(data).then(programsArray => {
+//     console.log(programsArray);
+//   });
+// });
